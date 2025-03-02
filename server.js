106f2baf6adb515198e79220eae0ac4bdce4fbf2
rashid ekbal <rtechdevlopment123@gmail.com>
2025-03-02 16:13:44 +0530

    add listening to client and  sending to another client

--- server.js
+++ server.js
@@ -7,22 +7,46 @@ import cors from "cors";
 
 let app = express();
 let server = http.createServer(app);
-let io = new Server(server, {
-  cors: { origin: "*" },
+export let io = new Server(server, {
+  cors: { origin: "*", credentials: true },
   transports: ["websocket", "polling"],
 });
+let onlineUsers = [];
+//add new online user
+function add_new_online_users(id, socket) {
+  onlineUsers.push({
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrLWKNduCw_8jK8-lg0p1K1t6X3ATRkhOkDg&s",
+    id: id,
+    Name: "User_" + id[0] + id[1] + id[2] + id[3] + id[4],
+    placeHolder: "something",
+  });
 
-server.listen(process.env.PORT || 8000, () => {
-  console.log(`server is running on port ${process.env.PORT || 8000}`);
-});
-
+  socket.broadcast.emit("updateUserList", onlineUsers);
+}
+//remove disconneted user
+function remove_disconneted_user(id, socket) {
+  onlineUsers = onlineUsers.filter((item) => {
+    return item.id != id;
+  });
+  socket.broadcast.emit("updateUserList", onlineUsers);
+}
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 io.on("connection", (user) => {
-  console.log("user connected");
+  user.emit("welcome", onlineUsers);
+  console.log(`new user with user id ${user.id} is connected`);
+  add_new_online_users(user.id, user);
 
   user.on("disconnect", () => {
-    console.log("user disconnected ");
+    console.log(`user of id ${user.id} just got disconnected `);
+    remove_disconneted_user(user.id, user);
   });
+  user.on("Messagep2pFromClient", (data) => {
+    io.to(data.id).emit("Messagep2pFromServer", { msg: data.msg, id: user.id });
+  });
+});
+server.listen(process.env.PORT || 8000, () => {
+  console.log(`server is running on port ${process.env.PORT || 8000}`);
 });
